4e98ea6fb5dd9756ae66056a1609b0280e7313a7
XSKW11111 <39919931+XSKW11111@users.noreply.github.com>
2019-06-07 22:43:46 +0800

    change path

--- Clusterization/retrieve_size.cpp
+++ Clusterization/retrieve_size.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     size_t vector_size;
-    ifstream file("/home/chuck/Documents/Test/entities.data", ios::binary);
+    ifstream file("/PATH/TO/ENTITIES.DATA", ios::binary);
     ofstream fileo("/PATH/TO/LOGS/num_entities");
     file.read((char *)& vector_size, sizeof(size_t));
     fileo << " num of entities : " << vector_size << endl;
