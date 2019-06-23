e1203332668df907f562a6da3a28ce34ed29a432
XSKW11111 <39919931+XSKW11111@users.noreply.github.com>
2019-06-23 21:38:14 +0800

    Update clusterization.cpp

--- Clusterization/clusterization.cpp
+++ Clusterization/clusterization.cpp
@@ -1,17 +1,17 @@
 #include "clusterization.hpp"
-#include <boost/progress.hpp>
+#include <./RDF_parser/progress_bar.hpp>
 #include <algorithm>
 #include <iostream>
 using namespace std;
 
 void cluster::clusterizing()
 {
-    cout << "clusterzing ... " << endl;
     if ((*this->reader).is_open())
     {
         size_t vector_size;
         (*this->reader).read((char *)& vector_size, sizeof(size_t));
-        boost::progress_display display(vector_size);
+        ProgressBar pbar("clusterzing ...", vector_size);
+        pbar.progress_begin();
         while(!(*this->reader).eof())
         {
             unsigned int tri_arr[3];
@@ -19,8 +19,9 @@ void cluster::clusterizing()
             ++cunt_entities[tri_arr[0]].cunt_entities;
             ++cunt_entities[tri_arr[2]].cunt_entities;
             join(tri_arr[0], tri_arr[2]);
-            ++display;
+            ++pbar.progress;
         }
+        pbar.progress_end();
     }
     else
     {
@@ -81,7 +82,8 @@ void cluster::log_cluster()
     size_t vector_size = us.size(); 
     unsigned int count = 0;
     unsigned int total = 0;
-    boost::progress_display display(vector_size);
+    ProgressBar pbar("log_cluster ...", vector_size);
+    pbar.progress_begin();
     for (int i = 0; i < vector_size; ++i)
     {
         
@@ -92,8 +94,9 @@ void cluster::log_cluster()
 
             total += cunt[i];
         }
-        ++display;
+        ++pbar.progress;
     }
+    pbar.progress_end();
     writer << "total : " << total;
     writer.close();
 
@@ -109,16 +112,22 @@ void cluster::log_entities_fre(bool ordered)
     unsigned int count = 0; 
     unsigned int total = 0; // count of entites's fre lower than 5
 
-    boost::progress_display display(vector_size);    
+    
+    
     if (ordered)
     {
+        cout << "sorting based on id";
+        sort(cunt_entities.begin(), cunt_entities.end(), [](Entities src, Entities des){return src.id < des.id;});
+        ProgressBar pbar("ordered cunt entities_fre", vector_size);
+        pbar.progress_begin();
         ofstream writer_2(save_path + "_cunt_e_o");
         for (int i = 0; i < vector_size; ++i)
         {   
             count++;
             writer_2 << count << "th cluster : id : " << cunt_entities[i].id << "\t frequency " << cunt_entities[i].cunt_entities << endl;
-            ++display;
+            ++pbar.progress;
         }
+        pbar.progress_end();
         writer_2.close();
     }
 
@@ -126,7 +135,8 @@ void cluster::log_entities_fre(bool ordered)
     sort(cunt_entities.begin(), cunt_entities.end(), [](Entities src, Entities des){return src.cunt_entities > des.cunt_entities;});
     
     count = 0;
-    display.restart(vector_size);   
+    ProgressBar pbar_1("unordered cunt entities_fre", vector_size); 
+    pbar_1.progress_begin(); 
     for (int i = 0; i < vector_size; ++i)
     {   
         count++;
@@ -136,8 +146,9 @@ void cluster::log_entities_fre(bool ordered)
             ++total;
             writer_1 << total << "th cluster id :" << cunt_entities[i].id << "\t frequency :" <<cunt_entities[i].cunt_entities << endl;
         }
-        ++display;
+        ++pbar_1.progress;
     }
+    pbar_1.progress_end();
     writer << "number of entites which fre less than 5 :" << total;
     writer.close();
     writer_1 << "number of entites which fre less than 5 :" << total;
