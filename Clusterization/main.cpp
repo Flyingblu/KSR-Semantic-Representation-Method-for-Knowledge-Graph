88f551bbe638aeb1e7cfe5f3a0618a6ad8ac74a7
XSKW11111 <39919931+XSKW11111@users.noreply.github.com>
2019-06-25 20:09:21 +0800

    Update main.cpp

--- Clusterization/main.cpp
+++ Clusterization/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     cluster* c = new cluster("/PATH/TO/TRIPLE.DATA", "/PATH/TO/LOGFILE", ENTITIES_SIZE);
     c->clusterizing();
-    c->logging(false, false, false, true);
+    c->logging(false, false, false, false, true);
     delete c;
     data_filter d_filter("LOAD_TRIPLEPATH", "LOAD_ENTITIES_PATH", "SAVE_PATH");
     d_filter.load();
